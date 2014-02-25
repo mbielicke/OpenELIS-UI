39856d7253ed83fa45c3b1590c0dfeae0aa85e4a
Travis Schmidt <travis.schmidt@gmail.com>
2014-02-25 15:25:27 +0000

    Set Table width to dropWidth for dropdown pop-up

--- src/org/openelis/ui/widget/Dropdown.java
+++ src/org/openelis/ui/widget/Dropdown.java
@@ -444,7 +444,7 @@ public class Dropdown<T> extends Composite implements ScreenWidgetInt, Queryable
         display.getCellFormatter().setWidth(0, 0, (width - 16) + "px");
         button.setWidth("16px");
 
-        if (table != null)
+        if (table != null && dropWidth == null)
             table.setWidth((width)+ "px");
 
     }
@@ -465,6 +465,9 @@ public class Dropdown<T> extends Composite implements ScreenWidgetInt, Queryable
 
     public void setDropWidth(String width) {
         dropWidth = width;
+        
+        if (table != null && dropWidth != null)
+            table.setWidth((dropWidth)+ "px");
     }
 
     public void setCase(TextBase.Case textCase) {
