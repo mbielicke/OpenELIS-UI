a7440ce8609ef1c586363251b06199a9ac804e59
tschmidt <tschmidt@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2014-04-22 16:14:14 +0000

    Commit of Scriptlet classes to UI

---
+++ OpenELIS/version2/UI/trunk/src/org/openelis/ui/scriptlet/ScriptletRunner.java
@@ -0,0 +1,40 @@
+package org.openelis.ui.scriptlet;
+
+import java.util.ArrayList;
+
+/**
+ * This class will run a set of Scriptlets on a Scriptlet object 
+ * and rerun them if one Scriptlets marks another pass is needed.
+ */
+public class ScriptletRunner<T extends ScriptletObject> {
+
+    private ArrayList<ScriptletInt<T>> scriptlets;
+    
+    public ScriptletRunner() {
+        scriptlets = new ArrayList<ScriptletInt<T>>();
+    }
+    
+    public void add(ScriptletInt<T>... scripts) {
+        for(ScriptletInt<T> script : scripts) 
+            scriptlets.add(script);
+    }
+    
+    public T run(T so) {
+        so.setStatus(ScriptletObject.Status.EXECUTING);
+        
+        for(ScriptletInt<T> script : scriptlets) {
+            so = script.run(so);
+            if(so.getStatus() == ScriptletObject.Status.FAILED)
+                return so;
+        }
+        
+        if(so.getRerun().size() > 0) {
+            so.setChanged(so.getRerun().remove(0));
+            run(so);
+        }
+        
+        so.setStatus(ScriptletObject.Status.FINISHED);
+ 
+        return so;
+    }
+}
