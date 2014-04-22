b2452ecd5e3c82a6ae31a95439450f122932a11c
Travis Schmidt <travis.schmidt@gmail.com>
2014-04-22 16:14:14 +0000

    Commit of Scriptlet classes to UI

---
+++ src/org/openelis/ui/scriptlet/ScriptletObject.java
@@ -0,0 +1,73 @@
+package org.openelis.ui.scriptlet;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ScriptletObject implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+        
+    public static enum Operation {ADD,UPDATE,DELETE};
+    
+    public static enum Status {EXECUTING,FINISHED,FAILED}
+    
+    protected Operation op;
+    
+    protected Status status;
+    
+    protected String changed;
+    
+    protected ArrayList<String> rerun;
+    
+    protected ArrayList<Exception> exceptions;
+   
+    
+    public ScriptletObject() {
+        rerun = new ArrayList<String>();
+    }
+        
+    public void setOperation(Operation op) {
+        this.op = op;
+    }
+    
+    public Operation getOperation() {
+        return op;
+    }
+    
+    public void addRerun(String... metas) {
+        for(String meta : metas)
+            rerun.add(meta);
+    }
+    
+    public ArrayList<String> getRerun() {
+        return rerun;
+    }
+    
+    public void setChanged(String changed) {
+        this.changed = changed;
+    }
+    
+    public String getChanged() {
+        return changed;
+    }
+    
+    public void addException(Exception e) {
+        if(exceptions == null) 
+            exceptions = new ArrayList<Exception>();
+        
+        exceptions.add(e);
+    }
+    
+    public ArrayList<Exception> getExceptions() {
+        return exceptions;
+    }
+    
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    
+    public Status getStatus() {
+        return status;
+    }
+
+}
