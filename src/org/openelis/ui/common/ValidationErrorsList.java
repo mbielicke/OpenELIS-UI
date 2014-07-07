620794fb91c99724f52bb2c43de0cdb964f2ceed
akampoow <akampoow@gmail.com>
2014-07-07 18:20:12 +0000

    Added a new class Caution for "overridable" warnings.  Added the warnings and cautions for inconsitent dates. Fixed the bug where the method of an analysis couldn't be changed more than once before the screen was reloaded with data.

--- src/org/openelis/ui/common/ValidationErrorsList.java
+++ src/org/openelis/ui/common/ValidationErrorsList.java
@@ -33,26 +33,30 @@ public class ValidationErrorsList extends Exception {
     
     private static final long serialVersionUID = 1L;
     private ArrayList<Exception> errors;
-    private boolean hasErrors, hasWarnings;
+    private boolean hasErrors, hasWarnings, hasCautions;
     
     public ValidationErrorsList() {
         super();
         errors = new ArrayList<Exception>();
         hasErrors = false;
         hasWarnings = false;
+        hasCautions = false;
     }
 
     public ValidationErrorsList(String msg) {
         super(msg);
         hasErrors = false;
         hasWarnings = false;
+        hasCautions = false;
     }
     
     public void add(Exception ex){
         errors.add(ex);
 
-        if(ex instanceof Warning)
+        if (ex instanceof Warning)
             hasWarnings = true;
+        else if (ex instanceof Caution)
+            hasCautions = true;
         else
             hasErrors = true;
     }
@@ -73,4 +77,7 @@ public class ValidationErrorsList extends Exception {
         return hasWarnings;
     }
     
+    public boolean hasCautions(){
+        return hasCautions;
+    }
 }
