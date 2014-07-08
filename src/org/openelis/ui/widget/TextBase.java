fc4b48098aa04fe98eaf1370e5eab7a3ccefd8d2
Travis Schmidt <travis.schmidt@gmail.com>
2014-07-08 15:08:28 +0000

    Fixed the TextBase bug where the first character in a masked box would not be deleted.

--- src/org/openelis/ui/widget/TextBase.java
+++ src/org/openelis/ui/widget/TextBase.java
@@ -200,10 +200,9 @@ public class TextBase extends Composite {
      * @param enforce
      */
     public void enforceMask(boolean enforce) {
+        enforceMask = enforce;
         if(mask == null)
             return;
-        
-    	enforceMask = enforce;
     	
     	if(enforce)
     	    box.setMaxLength(mask.length());
@@ -212,7 +211,7 @@ public class TextBase extends Composite {
     }
     
     public boolean isMaskEnforced() {
-    	return enforceMask;
+    	return mask != null && enforceMask;
     }
     
     protected void maskKeyDown(KeyDownEvent event) {
@@ -224,7 +223,7 @@ public class TextBase extends Composite {
 		/*
 		 * If return if mask is not be enforced such as when in Query Mode;
 		 */
-		if(!enforceMask)
+		if(!isMaskEnforced())
 			return;
 		
 		if((event.isAnyModifierKeyDown() && !event.isShiftKeyDown()) ||
@@ -281,7 +280,7 @@ public class TextBase extends Composite {
 			if(event.getNativeEvent().getKeyCode() == KeyCodes.KEY_BACKSPACE)
 				cursor--;
 
-			if(cursor  > 0) {
+			if(cursor  >= 0) {
 			
 			    mc = mask.charAt(cursor);  // get current mask char based on cursor
 
@@ -328,7 +327,7 @@ public class TextBase extends Composite {
 		/*
 		 * If return if mask is not be enforced such as when in Query Mode;
 		 */
-		if(!enforceMask)
+		if(!isMaskEnforced())
 			return;
 		
 		if((event.isAltKeyDown() || event.isControlKeyDown() || event.isMetaKeyDown()) ||
