944e4806c2759e303cc54f3bef8dc3388c0c60d4
Travis Schmidt <travis.schmidt@gmail.com>
2014-08-22 21:30:53 +0000

    Commit of FileDrop

---
+++ src/org/openelis/ui/widget/fileupload/FileDrop.java
@@ -0,0 +1,174 @@
+package org.openelis.ui.widget.fileupload;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.event.dom.client.DragEnterEvent;
+import com.google.gwt.event.dom.client.DragEnterHandler;
+import com.google.gwt.event.dom.client.DragOverEvent;
+import com.google.gwt.event.dom.client.DragOverHandler;
+import com.google.gwt.event.dom.client.DropEvent;
+import com.google.gwt.event.dom.client.DropHandler;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ *  This class is used to make a widget accept dropped files that can be uploaded to a URL. 
+ */
+public class FileDrop implements DropHandler, DragEnterHandler, DragOverHandler {
+	
+	protected FormData formData;
+	protected boolean sendAuto,enabled = true;
+	protected String sendUrl;
+	
+	/**
+	 * Constructor that accepts a widget to enable as an area to drop files.  The url param
+	 * is where the files will be uploaded to.
+	 * 
+	 * @param dropArea
+	 * @param url
+	 */
+	public FileDrop(Widget dropArea, String url) {
+		this(dropArea,url,true);
+	}
+	
+	/**
+	 * This constructor can be used to override the default behavior of sending files as soon
+	 * as they are dropped by passing false in the sendAuto param.  The send() method must then 
+	 * be called to upload the files
+	 * 
+	 * @param dropArea
+	 * @param url
+	 * @param sendAuto
+	 */
+	public FileDrop(Widget dropArea, String url, boolean sendAuto) {
+		formData = FormData.create();
+		this.sendAuto = sendAuto;
+		this.sendUrl = url;
+
+		dropArea.addDomHandler(this, DropEvent.getType());
+		
+		dropArea.addDomHandler(this, DragEnterEvent.getType());
+		
+		dropArea.addDomHandler(this, DragOverEvent.getType());
+	}
+
+	/**
+	 * This method is implemented as part of the DropHandler interface.  It
+	 * can be overridden by screens if necessary. 
+	 */
+	@Override
+	public void onDrop(DropEvent event) {
+		event.stopPropagation();
+		event.preventDefault();
+		
+		if(!enabled)
+			return;
+
+		File[] files = getFiles(event.getDataTransfer());
+		
+		if(sendAuto)
+			formData = FormData.create();
+		
+		for(File file : files) {
+			formData.append("file", file, file.name());
+		}
+		
+		if(sendAuto)
+			formData.send(sendUrl);
+	}
+	
+	/**
+	 * This method is implemented as part of the DragEnter interface.  It can be overridden 
+	 * to code some visual cue to the user to know the have entered a drop area 
+	 */
+	@Override
+	public void onDragEnter(DragEnterEvent event) {
+		event.preventDefault();
+		event.stopPropagation();
+		
+		if(!enabled)
+			return;
+	}
+	
+	@Override
+	public void onDragOver(DragOverEvent event ) {
+		event.preventDefault();
+		event.stopPropagation();
+		
+		if(!enabled)
+			return;
+	}
+	
+	/**
+	 * This method can be called to send all files that have been dropped to the url that 
+	 * that is set in sendUrl.  Only necessary if sendAuto has been set to false.
+	 */
+	public void send() {
+		assert sendUrl != null;
+		
+		formData.send(sendUrl);
+	}
+	
+	/**
+	 * This method can be called to send all files that have been dropped to the url that 
+	 * is set in sendUrl with a callback that will notify the caller of success or failure.
+	 * @param callback
+	 */
+	public void send(FormData.Callback callback) {
+		assert sendUrl != null;
+		
+		formData.send(sendUrl,callback);
+	}
+
+	/**
+	 * Method enables to disables the drop area
+	 * @param enabled
+	 */
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	/**
+	 * Returns true if the dropArea is enabled to receive dropped files
+	 * @return
+	 */
+	public boolean isEnabled() {
+		return enabled;
+	}
+	
+	/**
+	 * Sets the URL to send the dropped files to
+	 * @param url
+	 */
+	public void setSendUrl(String url) {
+		this.sendUrl = url;
+	}
+	
+	/**
+	 * Returns the currently set URL for the dropped files
+	 * @return
+	 */
+	public String getSendUrl() {
+		return sendUrl;
+	}
+	
+	/**
+	 * Pass true if the files should be sent as soon as they are dropped
+	 * @param sendAuto
+	 */
+	public void setSendOnDrop(boolean sendAuto) {
+		this.sendAuto = sendAuto;
+	}
+	
+	/**
+	 * Returns true if if the files will be sent on drop
+	 * @return
+	 */
+	public boolean isSendOnDrop() {
+		return sendAuto;
+	}
+	
+    protected static native File[] getFiles(JavaScriptObject transfer) /*-{
+    	return transfer.files;
+    }-*/;
+    	
+
+}
