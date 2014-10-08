ff14799d626ec4d9a1f9e08de83cea0acacab935
Travis Schmidt <travis.schmidt@gmail.com>
2014-10-08 14:53:40 +0000

    Fix for Checkbox auto centering when we don't want it to

--- src/org/openelis/ui/widget/CheckBox.java
+++ src/org/openelis/ui/widget/CheckBox.java
@@ -34,6 +34,7 @@ import org.openelis.ui.common.Util;
 import org.openelis.ui.common.data.QueryData;
 import org.openelis.ui.resources.CheckboxCSS;
 
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.BlurEvent;
 import com.google.gwt.event.dom.client.BlurHandler;
 import com.google.gwt.event.dom.client.FocusEvent;
@@ -142,6 +143,7 @@ public class CheckBox extends FocusPanel implements ScreenWidgetInt,
         
         exceptions = new Exceptions();
         
+        setWidth("13px");
     }
 
     /**
@@ -430,11 +432,13 @@ public class CheckBox extends FocusPanel implements ScreenWidgetInt,
     
     @Override
     public void setWidth(String width) {
+    	getElement().getStyle().setWidth(Util.stripUnits(width), Unit.PX);
     	check.setWidth(width);
     }
     
     @Override
     public void setHeight(String height) {
+    	getElement().getStyle().setHeight(Util.stripUnits(height), Unit.PX);
     	check.setHeight(height);
     }
 
