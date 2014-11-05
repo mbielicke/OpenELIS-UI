2d399f27b2d691beedde4175db4c1e6eb463a995
tschmidt <tschmidt@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2014-11-05 20:39:31 +0000

    Allow CheckboxCell to use different css

--- OpenELIS/version2/UI/trunk/src/org/openelis/ui/widget/table/CheckBoxCell.java
+++ OpenELIS/version2/UI/trunk/src/org/openelis/ui/widget/table/CheckBoxCell.java
@@ -254,6 +254,11 @@ public class CheckBoxCell implements CellEditor, CellRenderer, IsWidget, HasWidg
         return div;
 	}
 	
+	public void setCss(CheckboxCSS css) {
+		this.css = css;
+		css.ensureInjected();
+	}
+	
 	public void setAlign(String align) {
 	    this.align = align;
 	}
