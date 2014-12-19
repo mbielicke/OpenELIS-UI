258cb9000bd42b0a4e3199e8c43bce8d12bac085
tschmidt <tschmidt@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2014-12-19 16:08:59 +0000

    Fix to make Button positioning css only

--- OpenELIS/version2/UI/trunk/src/org/openelis/ui/widget/Button.java
+++ OpenELIS/version2/UI/trunk/src/org/openelis/ui/widget/Button.java
@@ -30,7 +30,6 @@ import org.openelis.ui.resources.IconCSS;
 import org.openelis.ui.resources.UIResources;
 import org.openelis.ui.widget.Balloon.Placement;
 
-import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.shared.GWT;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Document;
@@ -38,7 +37,6 @@ import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.dom.client.Style.Display;
 import com.google.gwt.dom.client.Style.OutlineStyle;
-import com.google.gwt.dom.client.Style.Position;
 import com.google.gwt.dom.client.Style.TextAlign;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.dom.client.Style.WhiteSpace;
@@ -81,7 +79,7 @@ public class Button extends FocusPanel implements ScreenWidgetInt, HasBalloon {
 	public static final ButtonUiBinder uiBinder = GWT.create(ButtonUiBinder.class);
 	
 	@UiField
-	protected DivElement left,center,right,outer,inner;
+	protected DivElement left,center,right,outer;
 	
     protected Balloon.Options   options;
     protected boolean toggles, enabled, pressed, locked;
@@ -105,8 +103,6 @@ public class Button extends FocusPanel implements ScreenWidgetInt, HasBalloon {
 		setCss(UIResources.INSTANCE.button());
 		getElement().getStyle().setOutlineStyle(OutlineStyle.NONE);
 		setEnabled(false);
-		//super.setWidth("auto");
-		//super.setHeight("auto");
 	}
 	
     public Button(String icon,String label) {
@@ -246,17 +242,8 @@ public class Button extends FocusPanel implements ScreenWidgetInt, HasBalloon {
 	}
 	
 	private void setButtonElement(final DivElement div, Element element) {
-		div.getStyle().setDisplay(Display.BLOCK);
-		div.getStyle().setPosition(Position.RELATIVE);
-		div.appendChild(element);
-
-		Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
-			@Override
-			public void execute() {
-				calcOffsetToCenter(outer,div);
-			}
-		});
-		
+		div.getStyle().setDisplay(Display.TABLE_CELL);
+		div.appendChild(element);		
 	}
 	
 	public void setDisabledImage(ImageResource imageResource) {
@@ -515,26 +502,5 @@ public class Button extends FocusPanel implements ScreenWidgetInt, HasBalloon {
     public HandlerRegistration addClickHandler(ClickHandler handler) {
     	return addHandler(handler,ClickEvent.getType());
     }
-    
-    private void calcOffsetToCenter(final Element div, final Element content) {
-    	Scheduler.get().scheduleIncremental(new Scheduler.RepeatingCommand() {
-			@Override
-			public boolean execute() {
-				if(isDrawn()) { 
-					content.getStyle().setTop(calcTop(div), Unit.PX);
-					return false;
-				}
-				return true;
-			}
-			
-			private boolean isDrawn() {
-				return isAttached() && div.getClientHeight() > 0 && content.getClientHeight() <= div.getClientHeight();
-			}
-			
-			private double calcTop(Element div) {
-				return (topOffset > 0)  ? topOffset : (div.getClientHeight() - content.getClientHeight()) / 2.0;
-			}
-		});
-    }
-    
+        
 }
