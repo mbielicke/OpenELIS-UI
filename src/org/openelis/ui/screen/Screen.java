58e503f714ac0fdcaf42bc942f388f86b1a7d3ef
Travis Schmidt <travis.schmidt@gmail.com>
2014-12-23 22:05:46 +0000

    Stopped screen from overriding a set key value in QueryData if set by a screen implementation

--- src/org/openelis/ui/screen/Screen.java
+++ src/org/openelis/ui/screen/Screen.java
@@ -252,7 +252,7 @@ public class Screen extends ResizeComposite implements FocusHandler, HasDataChan
                 for (int i = 0; i < qds.length; i++ )
                     list.add(qds[i]);
             } else if (query != null) {
-            	if(((QueryData)query).getKey() != null)
+            	if(((QueryData)query).getKey() == null)
             		((QueryData)query).setKey(key);
                 list.add((QueryData)query);
             }
