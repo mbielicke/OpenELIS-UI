e7698acaeeecd4b8af62fc0513612f5d0b35c3d9
Travis Schmidt <travis.schmidt@gmail.com>
2015-03-30 21:22:27 +0000

    Fix to try and remove error style first time

--- src/org/openelis/ui/widget/table/StaticView.java
+++ src/org/openelis/ui/widget/table/StaticView.java
@@ -449,7 +449,8 @@ public class StaticView extends ViewInt {
 
                     });
                 } else {
-                    flexTable.getCellFormatter().removeStyleName(r, c, Balloon.isWarning(table.getEndUserExceptions(r, c), table.getValidateExceptions(r, c)) ? css.InputWarning() : css.InputError());
+                    flexTable.getCellFormatter().removeStyleName(r, c, css.InputWarning());
+                    flexTable.getCellFormatter().removeStyleName(r, c, css.InputError());
                     flexTable.removeHandler(r, c);
                 }
 
