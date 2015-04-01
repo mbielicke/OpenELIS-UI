e94f786f16929779df122d5449fd35e28c858759
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2015-04-01 19:06:57 +0000

    Release Date: 2015-03-23. Added Patient Phone # to clinical sample screen/tabs. Added PT domain to verification report. Made description wrap on worksheet line list printouts. Bug fixes.

---
+++ OpenELIS/version2/UI/tags/OpenELIS_Production_3.0.1/src/org/openelis/ui/resources/TableCSS.java
@@ -0,0 +1,35 @@
+package org.openelis.ui.resources;
+
+import com.google.gwt.resources.client.CssResource;
+
+public interface TableCSS extends CssResource {
+	String Table();
+	String Selection();
+	String SubHeader();
+	String Header();
+	String First();
+	String Filtered();
+	String Sorted();
+	String ResizeCol();
+	String DropdownTable();
+	String Disabled();
+	String CellContainer();
+	String FilterButton();
+	String DropdownPopup();
+	String SelectionCell();
+	String Single();
+	
+	String TextboxSelected();
+	String TextboxUnselected();
+	String TableCalendar();
+	String WarnIcon();
+	String ErrorIcon();
+	String InputError();
+	String InputWarning();
+	String ScreenTableWithSides();
+	String TableWithPadding();
+	String GreenStatus();
+	String YellowStatus();
+	String RedStatus();
+	String LargeFont();
+}
