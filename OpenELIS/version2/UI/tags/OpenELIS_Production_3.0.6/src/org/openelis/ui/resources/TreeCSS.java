6052252f9024f5dbc8442e90968df5ecd93da86a
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2015-05-19 15:23:49 +0000

    Release Date: 2015-04-26. Changed the default billed_analytes from 0 to null in analysis_report_flags so that analyses with 0 reportable analytes get billed properly. Fixed output of risk_none and signs_none for ChlGcToCDCExport.

---
+++ OpenELIS/version2/UI/tags/OpenELIS_Production_3.0.6/src/org/openelis/ui/resources/TreeCSS.java
@@ -0,0 +1,15 @@
+package org.openelis.ui.resources;
+
+public interface TreeCSS extends TableCSS {
+	String Tree();
+	String Selection();
+	String InputError();
+	String InputWarning();
+	String TreeCell();
+	String TreeCellContainer();
+	String DropOnRow();
+	String GroupIcon();
+	String LeafIcon();
+	String treeClosedImage();
+	String treeOpenImage();
+}
---
+++ OpenELIS/version2/UI/tags/OpenELIS_Production_3.0.6/src/org/openelis/ui/widget/ButtonGroup.java
@@ -0,0 +1,94 @@
+package org.openelis.ui.widget;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.AcceptsOneWidget;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+
+/**
+ * This widget class implements a a panel of buttons where only one button can be clicked at a time.  An object that handles
+ * clicks for all the buttons in the group only needs to register once to this widget.  Used Mostly for AZButtons
+ * @author tschmidt
+ *
+ */
+public class ButtonGroup extends SimplePanel implements HasClickHandlers, ScreenWidgetInt {
+
+    protected ArrayList<Button> buttons = new ArrayList<Button>();
+    protected boolean enabled;
+
+    public ButtonGroup() {
+    }
+
+    /**
+     * Sets the panel of buttons to the group. The widget will walk through the Panel finding all the buttons registering
+     * them to the group.
+     * @param panel
+     */
+    public void setButtons(Panel panel) {
+    	super.setWidget(panel);
+        findButtons(panel);
+    }
+
+    private void findButtons(HasWidgets hw) {
+        Iterator<Widget> widsIt = hw.iterator();
+        while (widsIt.hasNext()) {
+            Widget wids = widsIt.next();
+            if (wids instanceof Button) {
+                buttons.add((Button)wids);
+            } else if (wids instanceof HasWidgets) {
+                findButtons((HasWidgets)wids);
+            }
+        }
+    }
+
+    /**
+     * Registers all buttons in this group to handler that is passed to the method
+     */
+    public HandlerRegistration addClickHandler(ClickHandler handler) {
+        for (Button button : buttons) {
+            button.addClickHandler(handler);
+        }
+        return null;
+    }
+
+    /**
+     * enables or disables the buttons in this group based on the boolean passed
+     * @param enabled
+     */
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+        for (Button button : buttons) {
+            button.setEnabled(enabled);
+        }
+    }
+
+	@Override
+	public void setWidget(IsWidget w) {
+		setButtons((Panel)w);
+	}
+	
+	@Override
+	public void setWidget(Widget w) {
+		setButtons((Panel)w);
+	}
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @Override
+    public void finishEditing() {
+        
+    }
+
+}
