74a8687cc6a5047bd79db4518d527f677e8f0cac
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2015-06-03 18:17:36 +0000

    Removed addValidOptions line again as it is throwing a NoSuchMethodException.

--- OpenELIS/version2/UI/trunk/src/org/openelis/login/OpenELISLDAPModule.java
+++ OpenELIS/version2/UI/trunk/src/org/openelis/login/OpenELISLDAPModule.java
@@ -43,7 +43,6 @@ public class OpenELISLDAPModule extends LdapExtLoginModule {
 
     @Override
     public void initialize(Subject subject, CallbackHandler callbackHandler, Map sharedState, Map options) {
-        addValidOptions(new String[] {"loginLockoutTime", "loginIPRetryCount", "loginNameRetryCount"});
         super.initialize(subject, callbackHandler, sharedState, options);
 
         if (options.containsKey("loginLockoutTime"))
