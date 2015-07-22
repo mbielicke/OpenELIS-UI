f0909c3f392c929b6f73dbb3c785ad139e2be520
mbielick <mbielick@3fcb8963-61b0-ac40-9c14-3465a65bc2ab>
2015-07-22 21:44:39 +0000

    Release Date: 2015-06-24. Modified ChlGcWorksheet scriptlet to requeue analyses with Equivocal results. Changed shading of headers on Final Report to be more readable when faxed. Changed EditNote screen to set the subject from standard notes on paste instead of selection. Bug fixes.

---
+++ OpenELIS/version2/UI/tags/OpenELIS_Production_3.0.9/src/org/openelis/ui/resources/TextCSS.java
@@ -0,0 +1,15 @@
+package org.openelis.ui.resources;
+
+import com.google.gwt.resources.client.CssResource;
+
+public interface TextCSS extends CssResource {
+	String Upper();
+	String Lower();
+	String Focus();
+	String InputError();
+	String InputWarning();
+	String ScreenTextBox();
+	String ScreenTextArea();
+	String ScreenLabel();
+
+}
