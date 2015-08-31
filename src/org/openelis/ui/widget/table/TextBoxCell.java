5f50c82817cfe90e7640a83f30766875a693ec9d
Travis Schmidt <travis.schmidt@gmail.com>
2015-08-31 19:15:43 +0000

    Fixed TextboxCell to set query mode correctly

--- src/org/openelis/ui/widget/table/TextBoxCell.java
+++ src/org/openelis/ui/widget/table/TextBoxCell.java
@@ -119,6 +119,7 @@ public class TextBoxCell implements CellRenderer, CellEditor, IsWidget, HasWidge
     @SuppressWarnings("rawtypes")
 	public void startEditing(Object value, Container container, NativeEvent event) {
     	editor.setQueryMode(false);
+    	query = false;
     	if(!editor.getHelper().isCorrectType(value))
     		editor.setText(DataBaseUtil.toString(value));
     	else 
